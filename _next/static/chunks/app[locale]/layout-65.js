79491e4ccb8e3194357023fc3c7ad3784ba654ab
Launchmeme <certoselenoi@gmail.com>
2026-08-31 00:32:11 +0200

    Update layout-65.js

--- _next/static/chunks/app[locale]/layout-65.js
+++ _next/static/chunks/app[locale]/layout-65.js
@@ -1,1 +1,18 @@
+import WalletContextProvider from "../components/WalletProvider";
 
+export const metadata = {
+  title: "LaunchMemes | Create Solana Tokens",
+  description: "Create Solana Tokens",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <WalletContextProvider>
+          {children}
+        </WalletContextProvider>
+      </body>
+    </html>
+  );
+}
